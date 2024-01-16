0420a1c3a24a50caa65f55a7ce4f0829ddd3ffb2
Ackerblad <johanackerblad@outlook.com>
2024-01-16 23:02:34 +0100

    Basic design of the windows

---
+++ MainMenu.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cookbook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        SurpriseMeWindow surpriseMeWindow = new SurpriseMeWindow();
+        MyFridgeWindow myFridgeWindow = new MyFridgeWindow();
+        IngredientsWindow ingredientsWindow = new IngredientsWindow();
+        RecipesWindow recipesWindow = new RecipesWindow();
+        
+        public MainMenu()
+        {
+            InitializeComponent();
+            this.Closed += Window_Closed;
+        }
+
+        private void Window_Closed(Object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SurpriseMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            surpriseMeWindow.MainMenu = this;
+            surpriseMeWindow.Show();
+            this.Hide();
+        }
+
+        private void MyFridgeButton_Click(object sender, RoutedEventArgs e)
+        {
+            myFridgeWindow.MainMenu = this;
+            myFridgeWindow.Show();
+            this.Hide();
+        }
+
+        private void IngredientsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ingredientsWindow.MainMenu = this;
+            ingredientsWindow.Show();
+            this.Hide();
+        }
+
+        private void RecipesButton_Click(object sender, RoutedEventArgs e)
+        {
+            recipesWindow.MainMenu = this;
+            recipesWindow.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ RecipesWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cookbook
+{
+    /// <summary>
+    /// Interaction logic for RecipesWindow.xaml
+    /// </summary>
+    public partial class RecipesWindow : Window
+    {
+        public MainMenu MainMenu { get; set; }
+
+        public RecipesWindow()
+        {
+            InitializeComponent();
+            this.Closed += Window_Closed;
+        }
+
+        private void Window_Closed(Object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu.Show();
+            this.Hide();
+        }
+    }
+}
